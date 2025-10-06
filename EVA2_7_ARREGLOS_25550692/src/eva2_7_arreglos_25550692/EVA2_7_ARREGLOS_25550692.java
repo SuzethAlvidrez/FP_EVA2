7b0b11c1b35ac9ae7b751d9cfa96c3921d37fe18
SuzethAlvidrez <L25550692@chihuahua2.tecnm.mx>
2025-10-06 16:03:02 -0600

    Add files via upload

---
+++ EVA2_7_ARREGLOS_25550692/src/eva2_7_arreglos_25550692/EVA2_7_ARREGLOS_25550692.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_7_arreglos_25550692;
+
+/**
+ *
+ * @author suzet
+ */
+public class EVA2_7_ARREGLOS_25550692 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String cade = "Hola mundo!!";
+        //String== arreglo
+        System.out.println(cade.charAt(0));
+        //caracter individual
+        char caracter = cade.charAt(11);
+        System.out.println(caracter);
+        System.out.println(cade.length());
+        
+    }
+    
+}
---
+++ EVA2_8_FOR_25550692/src/eva2_8_for_25550692/EVA2_8_FOR_25550692.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for_25550692;
+
+/**
+ *
+ * @author suzet
+ */
+public class EVA2_8_FOR_25550692 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // 3 parametros
+       //variable y valor de inicio
+       //hasta donde llegaas CONDICIÓN --> TIENE QUE SER VERDAD
+       //CUANDO ES FALSO EL FOR SE DETIENE
+       //incremento o decremento
+       //++ --
+       //i++ SIGNIFICA SUMARE 1 AL VALOR INICIAL
+        for(int i = 1; i <= 10; i++){
+            
+        System.out.println("HOLA MUNDO!!");
+        }
+    
+        for(int i = 10; i >= 1; i--){
+            
+        System.out.println(i);
+        }
+   
+    
+}}
