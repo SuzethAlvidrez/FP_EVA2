52358fe37e5a51e16c086df3cde80e20e8fece3a
SuzethAlvidrez <L25550692@chihuahua2.tecnm.mx>
2025-10-20 12:46:46 -0600

    Add files via upload

---
+++ EVA2_15_ACOMULADORES_25550692/src/eva2_15_acomuladores_25550692/EVA2_15_ACOMULADORES_25550692.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_15_acomuladores_25550692;
+
+/**
+ *
+ * @author suzet
+ */
+public class EVA2_15_ACOMULADORES_25550692 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //acumuladores
+        //++-- += -= *=
+        int cuenta = 0;
+        //ASIGNACIÓN -->  =
+        System.out.println(cuenta);
+        cuenta = cuenta  + 100;//acumulador
+        System.out.println(cuenta);
+        cuenta = cuenta + 1000;
+        System.out.println(cuenta);
+        cuenta = cuenta - 800;
+        System.out.println(cuenta);
+        cuenta += 2000;
+        System.out.println(cuenta);
+   }              
+    
+}
