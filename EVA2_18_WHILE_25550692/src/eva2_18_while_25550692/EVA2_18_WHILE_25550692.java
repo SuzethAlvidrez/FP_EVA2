582354dbc92de7e410b86e71f0f96188943788d3
SuzethAlvidrez <L25550692@chihuahua2.tecnm.mx>
2025-10-28 16:40:38 -0600

    Add files via upload

---
+++ EVA2_18_WHILE_25550692/src/eva2_18_while_25550692/EVA2_18_WHILE_25550692.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_18_while_25550692;
+
+/**
+ *
+ * @author suzet
+ */
+public class EVA2_18_WHILE_25550692 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 1;
+        while(i<=10){//se repite si la condicion es verdad
+            System.out.println("Suzzeth Avidrez");
+            i++;//estructura basica del while
+        }
+       //SIMULAR UN FOR CON UN DO WHILE
+        System.out.println("CICLO DO WHILE ---->");
+        i = 1;
+        do{
+        System.out.println("Suzzeth Avidrez");
+        i++;
+        }while(i<=10);
+    }
+    
+}
