230f768fd425872d43cb68dcf4efe3be98e6b5c3
SuzethAlvidrez <L25550692@chihuahua2.tecnm.mx>
2025-10-28 09:09:58 -0600

    Add files via upload

---
+++ EVA2_17_ALCANCE_25550692/src/eva2_17_alcance_25550692/EVA2_17_ALCANCE_25550692.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_17_alcance_25550692;
+
+/**
+ *
+ * @author suzet
+ */
+public class EVA2_17_ALCANCE_25550692 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int i = 5;
+        {
+            System.out.println(i);
+            int valor = 100;
+            System.out.println(valor);
+        } //valor aqui no existe 
+        System.out.println(valor);
+    }
+    
+}
